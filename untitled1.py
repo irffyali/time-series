cebdb777147eb5f998a170acf6b406def7f37198
irffyali <40044356+irffyali@users.noreply.github.com>
2019-09-16 17:29:08 +0100

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -10,7 +10,7 @@ from fbprophet import Prophet
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-df = pd.read_csv('C:/Users/irffy/OneDrive/Documents/Msc project files/MPS_LSOA_Level_Crime_Historic.csv')
+df = pd.read_csv('C:/Users/irffy/OneDrive/Documents/Msc project files/MPS_LSOA_Level_Crime_Historic.csv') #obtain csv form lin in read me change file 
 df['totals'] = df.iloc[:, 4:].sum(axis=0)
 months = list(df.columns)[4:]
 months.pop()
@@ -33,13 +33,13 @@ end = datetime.strptime(endDate, '%Y-%m-%d').date()
 while cur_date < end:
     print(cur_date)
     month.append(cur_date)
-    cur_date += relativedelta(months=1)
+    cur_date += relativedelta(months=1) #generate list of months as datetime objects.
     
 monthlydict = {'ds' : month,'y': monthlytotals}
 monthlydf = pd.DataFrame(monthlydict)
 
-timeseries = Prophet(seasonality_mode='multiplicative').fit(monthlydf)
-future = timeseries.make_future_dataframe(periods=36, freq = 'M')
+timeseries = Prophet(seasonality_mode='multiplicative').fit(monthlydf) #generate time series object for monthly data 
+future = timeseries.make_future_dataframe(periods=36, freq = 'M')#give monthly predictions for 36 months into the future
 fcst = timeseries.predict(future)
 fig2 = timeseries.plot(fcst)
 fig = timeseries.plot_components(fcst)
